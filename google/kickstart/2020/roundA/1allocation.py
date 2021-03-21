105ca447f79f6ff80e1149d27cc9567ea7df4b29
Juan Damian <jcarlosdamian@gmail.com>
2021-03-21 06:09:45 -0500

    kickstart

---
+++ google/kickstart/2020/roundA/1allocation.py
@@ -0,0 +1,17 @@
+t = int(input()) # read a line with a single integer
+for i in range(1, t + 1):
+  n, m = [int(s) for s in input().split(" ")] # read a list of integers, 2 in this case
+  a = [int(s) for s in input().split(" ")] # read ai
+  a.sort()
+  total = 0
+  costoTotal = 0
+  for precio in a:
+    costoTotal += precio
+    if costoTotal <= m :
+      total += 1
+    else:
+      break
+  
+  print("Case #{}: {}".format(i,total))
+
+
